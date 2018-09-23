f7c4b8299ebfe353f6e98691354153e18d1cc1a6
Dave Arnold <darnold@hashicorp.com>
2018-09-22 23:03:57 -0700

    initial add

---
+++ main.tf
@@ -0,0 +1,6 @@
+resource "tfe_sentinel_policy" "test" {
+  name = "check-version"
+  organization = "${var.organization}"
+  policy = "${file("${path.module}/policies/check_version.sentinel")}"
+  enforce_mode = "hard-mandatory"
+}
