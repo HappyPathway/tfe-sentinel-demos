35de4aa0d78503d72134f25dc8366cfadf82e338
Dave Arnold <darnold@hashicorp.com>
2018-09-27 10:26:49 -0700

    adding allowed versions

--- main.tf
+++ main.tf
@@ -1,7 +1,16 @@
 variable "organization" {}
 resource "tfe_sentinel_policy" "test" {
+  count = "${var.check_version ? 1 : 0}"
   name = "check-version"
   organization = "${var.organization}"
   policy = "${file("${path.module}/policies/check_version.sentinel")}"
+  enforce_mode = "hard-mandatory"
+}
+
+resource "tfe_sentinel_policy" "test" {
+  count = "${var.allowed_versions ? 1 : 0}"
+  name = "allowed-versions"
+  organization = "${var.organization}"
+  policy = "${file("${path.module}/policies/allowed_versions.sentinel")}"
   enforce_mode = "hard-mandatory"
 }
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "allowed_versions" {
+  default = false
+  type = "string"
+  description = "Enable Allowed Versions Check"
+}
+
+variable "check_version" {
+  default = false
+  type = "string"
+  description = "Enable Check Version Check"
+}
