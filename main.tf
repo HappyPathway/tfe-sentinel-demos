3e31f3937d6ed9cd599f0864b933937da7299301
Dave Arnold <darnold@hashicorp.com>
2018-09-27 14:16:26 -0700

    adding verify validator

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 variable "organization" {}
-resource "tfe_sentinel_policy" "test" {
+resource "tfe_sentinel_policy" "check_version" {
   count = "${var.check_version ? 1 : 0}"
   name = "check-version"
   organization = "${var.organization}"
@@ -7,7 +7,7 @@ resource "tfe_sentinel_policy" "test" {
   enforce_mode = "hard-mandatory"
 }
 
-resource "tfe_sentinel_policy" "test" {
+resource "tfe_sentinel_policy" "allowed_versions" {
   count = "${var.allowed_versions ? 1 : 0}"
   name = "allowed-versions"
   organization = "${var.organization}"
